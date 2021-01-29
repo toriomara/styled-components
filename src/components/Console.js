1129180cb8073668365958c057c1d030b35884a6
toriomara <budkevich.vladimir@gmail.com>
2021-01-29 10:21:42 +0300

    first commit

---
+++ src/components/Console.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react'
+import styled from 'styled-components'
+import Flex from "./Flex"
+import Line from "./Line";
+
+const StyledConsole = styled.textarea`
+  width: 100%;
+  height: 70vh;
+  background: black;
+  color: ${props => props.color || props.theme.colors.primary};
+  font-size: 1.5rem;
+  border: none;
+  resize: none;
+
+  &:focus {
+    outline: none;
+  }
+  @media ${props => props.theme.media.phone} {
+    border: 1px solid red;
+  }  
+  @media ${props => props.theme.media.tablet} {
+    border: 1px solid green;
+  }
+`
+
+
+const Console = ({color, ...props}) => {
+    const [lines, setLines] = useState(['C/Users/Documents'])
+    const onKeyPress = e => {
+        if (e.charCode == 13) {
+            setLines([...lines, 'C/Users/Documents'])
+        }
+    }
+    return (
+        <Flex>
+            <Flex direction={'column'} margin='0 10px'>
+                {lines.map(line =>
+                    <Line color={color}>{line}</Line>)}
+            </Flex>
+            <StyledConsole onKeyPress={onKeyPress} color={color} {...props}/>
+        </Flex>
+    )
+}
+
+export default Console;
---
+++ src/components/Line.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledLine = styled.div`
+  font-size: 1.5rem;
+  color: ${props => props.color || props.theme.colors.primary};
+`
+
+const Line = (props) => {
+    return <StyledLine {...props} />
+};
+
+export default Line
